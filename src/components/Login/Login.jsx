d8dabbba9396e084462b39c7e5ba28f492440c82
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-11 00:04:47 +0530

    favicon and title updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -129,7 +129,7 @@ const Login = () => {
                                 type="submit"
                                 className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
                             >
-                                Submit
+                                Login
                             </button>
                         </div>
                         <div className={`${styles.noramlFlex} w-full`}>
