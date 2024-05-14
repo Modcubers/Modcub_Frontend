408ad4bda81c4f35b27dc9dd11294ae81cdefa8e
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-14 23:55:46 +0530

    server link changed

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 // export const server = "https://backend-drab-ten.vercel.app/api/v2";
-export const server = "https://backend.modcub.in/api/v2";
+// export const server = "https://backend.modcub.in/api/v2";
+export const server="https://modcub-backend-1.onrender.com";
 
 // export const server = "http://localhost:8000/";
