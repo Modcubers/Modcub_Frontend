3e9170a3bcd0fe41e67dc4b50fe5e55c33410806
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-17 09:32:43 +0530

    added env in frontedn

--- src/server.js
+++ src/server.js
@@ -1,5 +1,1 @@
-// export const server = "https://backend-drab-ten.vercel.app/api/v2";
-// export const server = "https://backend.modcub.in/api/v2";
-export const server="https://stagingbackend.modcub.in/api/v2";
-
-// export const server = "http://localhost:8000/";
+export const server =process.env.REACT_APP_SERVER_URL;
